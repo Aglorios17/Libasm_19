488ea8d45fc681afbed5f1e918e954dfe1f4963d
Aglorios17 <alessio.glorioso17@gmail.com>
2020-03-25 16:56:59 +0100

    hello

---
+++ test/Hello_World.asm
@@ -0,0 +1,10 @@
+org 0x0100
+
+;ceci est un programme affichant un simple hello world
+
+mov dx, texte
+mov ah, 0x09
+int 0x21	; afficher le hello world
+ret		; fin du programme
+
+texte: db 'Hello, World !!', 10, 13, '$'
