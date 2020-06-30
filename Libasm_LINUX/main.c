b0bf4aae5dfa7bf16be1a9be6f565f146a913d64
aglorios17 <alessio.glorioso17@gmail.com>
2020-06-30 12:21:15 +0200

    adaptlinux

--- Libasm_LINUX/main.c
+++ Libasm_LINUX/main.c
@@ -5,15 +5,15 @@ int main()
 	char s1[40] = "salut";
 	char s2[40] = "lol";
 
-//	printf("%zu",ft_strlen("coucou"));
-//	printf("%d\n",ft_strcmp("coucou", "cucou"));
-//	printf("%d\n",strcmp("coucou", "cucou"));
-//	printf("%s\n",strcpy(s1, s2));
-//	printf("%s\n",ft_strcpy(s1, s2));
-//	printf("%s\n",strdup("yolo"));
-//	printf("%s\n",ft_strdup("yolo"));
-//	printf("%zd\n",write(2,"bonjour comment ca va",1));
-//	printf("%zd\n",ft_write(2,"bonjour comment ca va",1));
+	printf("%zu",ft_strlen("coucou"));
+	printf("%d\n",ft_strcmp("coucou", "cucou"));
+	printf("%d\n",strcmp("coucou", "cucou"));
+	printf("%s\n",strcpy(s1, s2));
+	printf("%s\n",ft_strcpy(s1, s2));
+	printf("%s\n",strdup("yolo les petits pote"));
+	printf("%s\n",ft_strdup("yolo les petits pote"));
+	printf("%zd\n",write(2,"bonjour comment ca va",1));
+	printf("%zd\n",ft_write(2,"bonjour comment ca va",1));
 ///////////////////////////////////////////////////////////////
 /*	int	fd;
 	char	buffer[40];
